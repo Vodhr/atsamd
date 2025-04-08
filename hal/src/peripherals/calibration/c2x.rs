6a087d337a52c8f6bf5a95266279b56f5bbdd8e0
Vodhr <post@benni-altenstein.de>
2025-04-08 10:47:45 +0200

    Added Osc48m calibration

--- hal/src/peripherals/calibration/c2x.rs
+++ hal/src/peripherals/calibration/c2x.rs
@@ -6,9 +6,9 @@ use core::ptr;
 // "The NVM Software Calibration Area can be read at address 0x00800080."
 const ADDR: u32 = 0x00806020;
 
-fn cal(addr_offset: u32, bit_shift: u32, bit_mask: u32) -> u32 {
+fn cal(addr_offset: u32, bit_shift: u32, bit_mask: u64) -> u64 {
     unsafe {
-        let addr: *const u32 = (ADDR + addr_offset) as *const _;
+        let addr: *const u64 = (ADDR + addr_offset) as *const _;
         let value = ptr::read(addr);
 
         (value >> bit_shift) & bit_mask
@@ -32,4 +32,15 @@ pub fn adc1_biascomp_scale_cal() -> u8 {
 /// ADC1 BIASREFBUF calibration value. Should be written to ADC1 CALIB register.
 pub fn adc1_biasref_scale_cal() -> u8 {
     cal(0, 6, 0b111) as u8
+}
+
+pub fn osc32k_cal() -> u8 {
+    cal(0, 12, 0b1111111) as u8
+}
+
+pub fn osc48m_5v_cal() -> u32 {
+    cal(0, 19, 0b111111_11111111_11111111) as u32
+}
+pub fn osc48m_3v3_cal() -> u32 {
+    cal(0, 41, 0b111111_11111111_11111111) as u32
 }
